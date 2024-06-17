bc1a953ad0d4ac7819751d776b953960bb774bab
shiva sharma <shivasharmajpr2003@gmail.com>
2024-06-17 19:38:50 +0530

    Theme toggle Implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
 export default function Home() {
   return (
-    <div className="font-extrabold">This should be Protected Route</div>
+    <div className="font-extrabold">
+      <ModeToggle />
+    </div>
   );
 }
