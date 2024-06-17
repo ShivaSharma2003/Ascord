8cd3218d64394d5044f271a9da8c1737af94772f
shiva sharma <shivasharmajpr2003@gmail.com>
2024-06-17 19:25:07 +0530

    Authentication Implemented with cleark

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// Define routes that should be protected
+const isProtectedRoute = createRouteMatcher([
+  "/", // Add any additional routes here
+]);
+// Update clerkMiddleware to manually protect routes
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) {
+    auth().protect(); // Protect the route if it matches the defined criteria
+  }
+});
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
